b6c02be6994b86022fe1477ad089deefadf0c126
PANKAJ2003 <pankajmishra84454@gmail.com>
2025-09-05 17:10:05 +0530

    initial commit

---
+++ patient-service/src/main/java/com/pms/patientservice/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.pms.patientservice.enums;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
---
+++ patient-service/src/main/java/com/pms/patientservice/model/Patient.java
@@ -0,0 +1,43 @@
+package com.pms.patientservice.model;
+
+import com.pms.patientservice.enums.Gender;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registedDate;
+
+    @NotNull
+    private Gender gender;
+
+}
